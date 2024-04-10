1a36df75a6d5959a119048a1316740325cb67f4c
Mariline Luccisano <y.marilineluccisano@outlook.it>
2024-04-10 12:05:23 +0200

    Aggiunte card piccole dinamiche in home

---
+++ ArciteatroVibo/Controllers/CardsController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using ArciteatroVibo.Models;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ArciteatroVibo.Controllers
+{
+    public class CardsController : Controller
+    {
+        private readonly ArciteatroViboValentiaContext _context;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public CardsController(ArciteatroViboValentiaContext context, IWebHostEnvironment hostingEnvironment)
+        {
+            _context = context;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        // GET: Cards
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Cards.ToListAsync());
+        }
+
+
+        public IActionResult PartialCard()
+        {
+            var cards = _context.Cards.ToList(); // se itero devo sempre avere una lista
+            return PartialView("_PartialCards", cards);
+        }
+
+        // GET: Cards/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var card = await _context.Cards
+                .FirstOrDefaultAsync(m => m.IdCard == id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            return View(card);
+        }
+
+        // GET: Cards/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cards/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdCard,Foto,Titolo,FotoCard")] Card card)
+        {
+            ModelState.Remove("Foto");
+           
+
+            if (ModelState.IsValid)
+            {
+                if (card.FotoCard != null && card.FotoCard.Length > 0)
+                {
+                    var path = Path.Combine(_hostingEnvironment.WebRootPath, "immagini", card.FotoCard.FileName);
+
+                    System.Diagnostics.Debug.WriteLine("sdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",path);
+
+                    using (var Filestream = new FileStream(path, FileMode.Create))
+                    {
+                        await card.FotoCard.CopyToAsync(Filestream);
+                    }
+
+                    card.Foto = "/immagini/" + card.FotoCard.FileName;
+                }
+
+                _context.Add(card);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(card);
+        }
+
+        // GET: Cards/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var card = await _context.Cards.FindAsync(id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+            return View(card);
+        }
+
+        // POST: Cards/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdCard,Foto,Titolo")] Card card)
+        {
+            if (id != card.IdCard)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(card);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CardExists(card.IdCard))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(card);
+        }
+
+        // GET: Cards/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var card = await _context.Cards
+                .FirstOrDefaultAsync(m => m.IdCard == id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            return View(card);
+        }
+
+        // POST: Cards/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var card = await _context.Cards.FindAsync(id);
+            if (card != null)
+            {
+                _context.Cards.Remove(card);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CardExists(int id)
+        {
+            return _context.Cards.Any(e => e.IdCard == id);
+        }
+    }
+}
