8d50e821a9f812e85f1fc3119f93be2fdd48f2f5
Mariline Luccisano <y.marilineluccisano@outlook.it>
2024-04-26 23:53:36 +0200

    Aggiunta pagina Registrazione

--- ArciteatroVibo/Models/Utenti.cs
+++ ArciteatroVibo/Models/Utenti.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ArciteatroVibo.Models;
 
@@ -8,13 +9,13 @@ public partial class Utenti
     public int IdUtente { get; set; }
 
     public bool? Newsletter { get; set; }
-
+    [Required(ErrorMessage = "Il Nome è obbligatorio.")]
     public string Nome { get; set; } = null!;
-
+    [Required(ErrorMessage = "Il Cognome è obbligatorio.")]
     public string Cognome { get; set; } = null!;
-
+    [Required(ErrorMessage = "la Email è obbligatoria.")]
     public string Email { get; set; } = null!;
-
+    [Required(ErrorMessage = "la Password è obbligatoria.")]
     public string Password { get; set; } = null!;
 
     public bool Ruolo { get; set; }
