661c2a2744b2ffb40193841ebdf8dbd7404054f1
Mariline Luccisano <y.marilineluccisano@outlook.it>
2024-04-29 18:06:44 +0200

    Aggiunto Edit Home1

--- ArciteatroVibo/Controllers/Home1Controller.cs
+++ ArciteatroVibo/Controllers/Home1Controller.cs
@@ -206,8 +206,10 @@ namespace ArciteatroVibo.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("IdHome1,Foto1,Foto2,Foto3,Upload,Video")] Home1 home1)
+        public async Task<IActionResult> Edit(int id, [Bind("IdHome1,Foto2,Foto3,Upload,Video,updateimm, updateimmDue, updateimmTre,UploadUp,Uploadvideo")] Home1 home1)
         {
+            ModelState.Remove("Foto1");
+
             if (id != home1.IdHome1)
             {
                 return NotFound();
@@ -215,26 +217,101 @@ namespace ArciteatroVibo.Controllers
 
             if (ModelState.IsValid)
             {
-                try
+                if (home1.updateimm != null && home1.updateimm.Length > 0)
                 {
-                    _context.Update(home1);
-                    await _context.SaveChangesAsync();
+                    var path = Path.Combine(_hostingEnvironment.WebRootPath,"immagini", home1.updateimm.FileName);
+
+                    using (var Filestream = new FileStream(path, FileMode.Create))
+                    {
+                        await home1.updateimm.CopyToAsync(Filestream);
+                    }
+
+                    home1.Foto1 = "/immagini/" + home1.updateimm.FileName;
                 }
-                catch (DbUpdateConcurrencyException)
+
+                if (home1.updateimmDue != null && home1.updateimmDue.Length > 0)
                 {
-                    if (!Home1Exists(home1.IdHome1))
+                    var path = Path.Combine(_hostingEnvironment.WebRootPath,"immagini", home1.updateimmDue.FileName);
+
+                    using (var Filestream = new FileStream(path, FileMode.Create))
                     {
-                        return NotFound();
+                        await home1.updateimmDue.CopyToAsync(Filestream);
                     }
-                    else
+
+                    home1.Foto2 = "/immagini/" + home1.updateimmDue.FileName;
+                }
+
+                if (home1.updateimmTre != null && home1.updateimmTre.Length > 0)
+                {
+                    var path = Path.Combine(_hostingEnvironment.WebRootPath,"immagini", home1.updateimmTre.FileName);
+
+                    using (var Filestream = new FileStream(path, FileMode.Create))
                     {
-                        throw;
+                        await home1.updateimmTre.CopyToAsync(Filestream);
+                    }
+
+                    home1.Foto3 = "/immagini/" + home1.updateimmTre.FileName;
+
+                    
+                }
+                if (home1.UploadUp != null && home1.UploadUp.Length > 0)
+                {
+                    var provider = new FileExtensionContentTypeProvider();
+
+                    // Add new mappings
+                    provider.Mappings[".pdf"] = "application/pdf";
+
+                    // Verifica del tipo MIME per assicurarsi che sia un file PDF
+                    string contentType;
+                    if (provider.TryGetContentType(home1.UploadUp.FileName, out contentType) && contentType == "application/pdf")
+                    {
+                        // Salva il file PDF nel percorso desiderato
+                        var pdfPath = Path.Combine(_hostingEnvironment.WebRootPath, "immagini/Statuto/", home1.UploadUp.FileName);
+
+                        using (var fileStream = new FileStream(pdfPath, FileMode.Create))
+                        {
+                            await home1.UploadUp.CopyToAsync(fileStream);
+                        }
+
+                        // Assegna il percorso del file PDF al modello
+                        home1.Upload = home1.UploadUp.FileName;
                     }
                 }
+                if (home1.Uploadvideo != null && home1.Uploadvideo.Length > 0)
+                {
+                    var provider = new FileExtensionContentTypeProvider();
+
+                    // Add new mappings
+                    provider.Mappings[".mp4"] = "video/mp4";
+
+                    // Verifica del tipo MIME per assicurarsi che sia un file PDF
+                    string contentType;
+
+                    if (provider.TryGetContentType(home1.Uploadvideo.FileName, out contentType) && contentType == "video/mp4")
+                    {
+                        // Salva il file PDF nel percorso desiderato
+                        var pdfPath = Path.Combine(_hostingEnvironment.WebRootPath, "immagini/video/", home1.Uploadvideo.FileName);
+
+                        using (var fileStream = new FileStream(pdfPath, FileMode.Create))
+                        {
+                            await home1.Uploadvideo.CopyToAsync(fileStream);
+                        }
+
+                        // Assegna il percorso del file PDF al modello
+                        home1.Video = home1.Uploadvideo.FileName;
+                    }
+                }
+                else
+                {
+                    home1.Foto1 = "/img/alla.jpeg";
+                }
+                _context.Update(home1);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(home1);
         }
+
 
         // GET: Home1/Delete/5
         public async Task<IActionResult> Delete(int? id)
