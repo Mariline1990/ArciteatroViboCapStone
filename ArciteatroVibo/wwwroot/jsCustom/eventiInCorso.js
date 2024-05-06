8a2a86b868e162129195e0112b21d7ded3fba7ad
Mariline Luccisano <y.marilineluccisano@outlook.it>
2024-05-06 14:36:55 +0200

    Modifiche Pagina Eventi

---
+++ ArciteatroVibo/wwwroot/jsCustom/eventiInCorso.js
@@ -0,0 +1,18 @@
+﻿
+
+var sfondo = document.getElementById("SfondoEvento");
+
+
+function Backgroud() {
+    if (window.scrollY >= sticky) {
+        navbarCustom.classList.add("stickyNav")
+        navbarCustom.classList.add("border-3")
+    } else {
+        navbarCustom.classList.remove("stickyNav")
+        navbarCustom.classList.remove("border-3");
+    }
+}
+var position = document.getElementById("navbarCustom");
+
+
+
